c514c31ff37dc2092a5405121b0e0d55d129e3de
Huyd <huyidada@gmail.com>
2018-07-04 09:16:36 +0800

    外部Tomcat

--- src/main/java/com/huyd/ManageApplication.java
+++ src/main/java/com/huyd/ManageApplication.java
@@ -3,12 +3,25 @@ package com.huyd;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan(basePackages = "com.huyd.domain")
 @SpringBootApplication
-public class ManageApplication {
+public class ManageApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManageApplication.class, args);
+//	public static void main(String[] args) {
+//		SpringApplication.run(ManageApplication.class, args);
+//	}
+public static void main(String[] args) {
+	SpringApplication.run(applicationClass, args);
+}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
 	}
+
+	private static Class<ManageApplication> applicationClass = ManageApplication.class;
+
 }
